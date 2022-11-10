a07918900f0a84b1cd7a124d9c9fc005834fea90
Nyandago <paulmutator@gmail.com>
2022-11-10 20:25:19 +0300

    MajiActivity.kt

--- app/src/main/java/com/cannybits/cannyapicall/MainActivity.kt
+++ app/src/main/java/com/cannybits/cannyapicall/MainActivity.kt
@@ -2,10 +2,39 @@ package com.cannybits.cannyapicall
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.create
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getUsers()
+    }
+
+    private fun getUsers() {
+
+        val retro = Retro().getRetroClient().create(MajiDataApi::class.java)
+
+
+        retro.getUsers().enqueue(object : Callback<List<MajiDataModel>>{
+            override fun onResponse(
+                call: Call<List<MajiDataModel>>,
+                response: Response<List<MajiDataModel>>
+            ) {
+                for (q in response.body()!!){
+                    Log.e("Oyooo",q.username.toString())
+                }
+            }
+
+            override fun onFailure(call: Call<List<MajiDataModel>>, t: Throwable) {
+                Log.e("Error","Hazijafikaa")
+            }
+
+        })
     }
 }
