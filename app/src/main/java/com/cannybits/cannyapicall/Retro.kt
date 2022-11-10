f12dc9e3a9cc0509164ed5c98af758a3b105e7fa
Nyandago <paulmutator@gmail.com>
2022-11-10 19:32:39 +0300

    Retro.kt

---
+++ app/src/main/java/com/cannybits/cannyapicall/Retro.kt
@@ -0,0 +1,16 @@
+package com.cannybits.cannyapicall
+
+import com.google.gson.GsonBuilder
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class Retro {
+
+    fun getRetroClient(): Retrofit{
+        val gson = GsonBuilder().setLenient().create()
+        return Retrofit.Builder()
+                    .baseUrl("http://majidatalvb.co.tz/api/")
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .build()
+    }
+}
