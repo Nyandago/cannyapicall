922f56cf2c79f58ec0594b901430fd5a4c50392e
Nyandago <paulmutator@gmail.com>
2022-11-11 16:28:03 +0300

    MajiDataApi.kt

--- app/src/main/java/com/cannybits/cannyapicall/MajiDataApi.kt
+++ app/src/main/java/com/cannybits/cannyapicall/MajiDataApi.kt
@@ -1,10 +1,21 @@
 package com.cannybits.cannyapicall
 
 import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface MajiDataApi {
     @GET("users")
     fun getUsers(): Call<List<MajiDataModel>>
+
+
+    @FormUrlEncoded
+    @POST("users/login")
+    fun userLogin(
+        @Field("username") username:String,
+        @Field("password") password: String
+    ):Call<List<MajiDataModel>>
 
 }
